03218fa6713d0d5a3328b576672752d6250aa90c
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-02 00:15:25 +0900

    useEffect used

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -1,6 +1,15 @@
 import React, {useEffect,useState} from 'react';
 
+import {getCoin} from '../services/api';//API
+
 const Landing = () => {
+    useEffect(()=>{
+        const fetchAPI = async()=>{
+         const data = await getCoin();
+         console.log(data)
+        }
+        fetchAPI();
+    },[])
     return (
         <div>
             landing 
