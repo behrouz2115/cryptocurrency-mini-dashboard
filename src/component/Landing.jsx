c7bbd2251f9a7b6fb330ee26044604e0214051f2
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-02 00:28:31 +0900

    show up coins in landing page

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -3,16 +3,25 @@ import React, {useEffect,useState} from 'react';
 import {getCoin} from '../services/api';//API
 
 const Landing = () => {
+
+    const [coins,setCoins] = useState([]);
+
     useEffect(()=>{
         const fetchAPI = async()=>{
          const data = await getCoin();
-         console.log(data)
+         setCoins(data)
         }
         fetchAPI();
     },[])
+
     return (
         <div>
-            landing 
+            <input type="text" placeholder="Search" />
+            <div>
+                {
+                    coins.map(coin=><p key={coin.id}>{coin.name}</p>)
+                }
+            </div>
         </div>
     );
 };
