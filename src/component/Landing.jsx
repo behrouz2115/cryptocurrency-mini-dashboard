ddada21eca4d888e5d286efe48008990cf3efec3
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-02 17:08:01 +0900

    show up data

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -2,6 +2,7 @@ import React, {useEffect,useState} from 'react';
 
 import {getCoin} from '../services/api';//API
  import Loader from './Loader';
+ import Coin from './Coin';
 
 const Landing = () => {
 
@@ -10,6 +11,7 @@ const Landing = () => {
     useEffect(()=>{
         const fetchAPI = async()=>{
          const data = await getCoin();
+         console.log(data)
          setCoins(data)
         }
         fetchAPI();
@@ -22,8 +24,15 @@ const Landing = () => {
                 coins.length?
             <div>
                 {
-                    coins.map(coin=><p key={coin.id}>{coin.name}</p>)
-                }
+                    coins.map(coin=><Coin
+                                          key={coin.id}
+                                          name={coin.name}
+                                          image={coin.image}
+                                          symbol={coin.symbol}
+                                          price={coin.current_price}
+                                          marketCap={coin.market_cap}
+                                          priceChange={coin.price_change_percentage_24h}/>)
+                }           
             </div>:<Loader/>
             }
         </div>
